4c59ce7888767f5b0584ca1e0890b001f9bb24af
Marzena <marzena.szopinska090@gmail.com>
2019-10-17 18:09:52 +0200

    user now is able to create new quotes and new quotes display to the screen

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <new-quote v-on:newQuoteCreated="quotes.push($event)"></new-quote>
     <!-- pass the array of quotes to quote grid component, this components is responsible for rendering each quote that exists -->
     <quote-grid v-bind:quotes="quotes"></quote-grid>
   </div>
@@ -7,6 +8,7 @@
 
 <script>
 import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
 
 export default {
   data: function() {
@@ -16,10 +18,17 @@ export default {
       // array of quotes
       quotes: ["First Quote"]
     };
+  }, // or instead v-on:newQuoteCreated="quotes.push($event)" you can simpy to it this way
+  methods: {
+    newQuote(newQuote) {
+      // add freshly created quote to quotes array
+      this.quotes.push(newQuote);
+    }
   },
   components: {
     // local set up for components
-    quoteGrid: QuoteGrid
+    quoteGrid: QuoteGrid,
+    newQuote: NewQuote
   }
 };
 </script>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote:</label>
+        <textarea class="form-control" rows="3" v-model="quote"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-primary" v-on:click.prevent="createNewQuote">Add Quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createNewQuote() {
+      // add custom event (emit) to send that information to the parent and add to an array
+      this.$emit("newQuoteCreated", this.quote);
+      // reset quote
+      this.quote = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
